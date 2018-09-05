978b8e1ef979b6c42635994688388d842790b65a
nicholettesams <nicholettesams@msn.com>
2018-09-05 09:49:46 -0400

    Split displayEmotions function, clean up code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,8 +5,38 @@ var offset = 0;
 var emotionName = "";
 var append = false;
 
-// Generic function for capturing the emotion name from the data-attribute
-function displayEmotions() {
+//Function for appending gifs and metadata to the page
+function displayEmotions(results) {
+    $("#emotions").empty();
+    for (var i = 0; i < results.length; i++){
+        //if appropriate rating add to page
+        if (results[i].rating !== "r") {  
+            //create image and set the source to be the still
+            var figure = $("<figure>")
+            //BONUS: List additional metadata
+            var caption = $("<figcaption>").html("<strong>Title: </strong>" + results[i].title + "<br><strong>Rating: </strong>" + results[i].rating)
+
+            //create image tag
+            var image = $("<img>").attr("src", results[i].images.fixed_height_still.url);
+
+            //add class to use in the on click event later
+            image.addClass("gif")
+
+            //add attribut to store the still and moving gif for later
+            image.attr("gifURL", results[i].images.fixed_height.url)
+            image.attr("stillURL", results[i].images.fixed_height_still.url)
+
+            // Appending the image to the figure 
+            figure.append(caption);
+            figure.append(image);
+
+            $("#emotions").append(figure)
+        }
+    }
+}
+
+// Function for capturing the emotion name from the data-attribute and searching giphy
+function searchEmotions() {
     //get the name of the emotion that was clicked
     emotionName = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + emotionName.replace(' ', '+') + apiKey + "&offset=" + offset;
@@ -16,45 +46,15 @@ function displayEmotions() {
       method: "GET"
     })
     .then(function(response){
-        //Append gifs and ratings 
-        //Need to figure out a way to store the moving gif 
-        //in addition to the still gif to be able to swap out later
-
+        //display results on the page
         var results = response.data
+        displayEmotions(results)
 
-        $("#emotions").empty();
-        for (var i = 0; i < response.data.length; i++){
-            //if appropriate rating add to page
-            if (response.data[i].rating !== "r") {  
-                //create image and set the source to} be the still
-                var figure = $("<figure>")
-                //BONUS: List additional metadata
-                var caption = $("<figcaption>").html("<strong>Title: </strong>" + results[i].title + "<br><strong>Rating: </strong>" + response.data[i].rating)
-
-                //create image tag
-                var image = $("<img>").attr("src", results[i].images.fixed_height_still.url);
-
-                //add class to use in the on click event later
-                image.addClass("gif")
-
-                //add attribut to store the still and moving gif for later
-                image.attr("gifURL", results[i].images.fixed_height.url)
-                image.attr("stillURL", results[i].images.fixed_height_still.url)
-
-                // Appending the image to the figure 
-                figure.append(caption);
-                figure.append(image);
-
-                $("#emotions").append(figure)
-            }
-        }
-        
     });
   }
 
   function runGif() {
-        //get the current source
-        var newURL = "";
+        //get the current source and attributes
         var source = $(this).attr("src");
         var gifURL = $(this).attr("gifURL"); 
         var stillURL = $(this).attr("stillURL");
@@ -81,7 +81,7 @@ function displayEmotions() {
       var button = $("<button>");
       // Adding a class of topic to our button that will be used later in the click event
       button.addClass("emotion");
-      // Adding a data-attribute which will be used later in displayEmotions()
+      // Adding a data-attribute which will be used later in searchEmotions()
       button.attr("data-name", topics[i]);
       // Providing the initial button text
       button.text(topics[i]);
@@ -98,19 +98,22 @@ function displayEmotions() {
     // This line grabs the input from the textbox
     var emotion = $("#emotion-input").val().trim();
 
-    if (emotion != ""){
+    if (emotion != "" && !topics.includes(emotion)){
         // Adding the topic from the textbox to the array
         topics.push(emotion);
 
         // Calling renderButtons which handles the processing of the topic array
         renderButtons();
     }
+
+    //clear out the field
+    $("#emotion-input").val("");
   });
 
   // Function for displaying the gifs
   // Click event listener to all elements with the class "emotion"
   // Adding the event listener to the parent div because it will work for dynamically generated elements
-  $("#emotion-buttons").on("click", ".emotion", displayEmotions);
+  $("#emotion-buttons").on("click", ".emotion", searchEmotions);
 
   // Function for running the gifs
   // Click event listener to all elements with the class "gif"
